986a13fa64b052fb627d6521e164446840e07d19
nariomasahisa <nariomasahisa@gmail.com>
2022-05-01 00:01:31 +0900

    データベースカラム名変更

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,6 +4,8 @@ from statistics import mode
 from django.db import models
 
 class Portfolio(models.Model):
+
+  # テーブルのカラムの設定
   id = models.CharField(max_length=48, unique=True, primary_key=True, null=False)
   code = models.CharField(max_length=4, null=False)
   name = models.CharField(max_length=50, null=False)
@@ -13,7 +15,7 @@ class Portfolio(models.Model):
   devidend = models.FloatField(default=0, null=False)
   buy_price = models.IntegerField(default=0, null=False)
   total_profit = models.IntegerField(default=0, null=False)
-  bool_value = models.IntegerField(default=0, null=False)
+  book_value = models.IntegerField(default=0, null=False)
   total_buy_price = models.IntegerField(default=0, null=False)
   profit_yield = models.FloatField(default=0, null=False)
   roe = models.FloatField(default=0, null=False)
