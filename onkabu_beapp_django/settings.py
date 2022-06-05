09db40258a29b1f66821c5e70659a1c542aafcc5
nariomasahisa <nariomasahisa@gmail.com>
2022-06-05 23:13:01 +0900

    setting.pyの修正(calc-valueを新しく追加したため)

--- onkabu_beapp_django/settings.py
+++ onkabu_beapp_django/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'onkabu.apps.OnkabuConfig',
     'portfolio.apps.PortfolioConfig',
+    'calc_value.apps.CalcValueConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
