9e250d61c1fc8c67fdbe1383d8016470012fb78d
nariomasahisa <nariomasahisa@gmail.com>
2022-06-05 23:13:19 +0900

    routing設定

--- onkabu_beapp_django/urls.py
+++ onkabu_beapp_django/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('portfolio/', include('portfolio.urls')),
     path('onkabu/', include('onkabu.urls')),
+    path('calc-value/', include('calc_value.urls'))
 ]
